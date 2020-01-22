eeaaf52df02d696e06413b73cc00d6bdf23c5e6f
jborycz <amanofsorrows@gmail.com>
2020-01-22 10:47:27 -0600

    Coordinates and loadings file added
    The all_loadings.csv file contains the coordinates, loadings, and rotated loadings (via varimax) for the data in question. The mfa_surveys_combined_v001.R script shows how to calculate the coordinates, loadings, and rotated loadings on lines 308-213.

--- mfa_surveys_combined_v001.R
+++ mfa_surveys_combined_v001.R
@@ -304,18 +304,26 @@ surveys_combined.mfa <- MFA(surveys_combined_limited,
 #  geom_bar(aes(x=surveys_combined.mfa$quali.var$coord[,0]))
 unrotated_plot_dim1 <- barplot(surveys_combined.mfa$quali.var$coord[,1], las=2)
 unrotated_plot_dim2 <- barplot(surveys_combined.mfa$quali.var$coord[,2], las=2)
+coords <- cbind(surveys_combined.mfa$quali.var$coord[,1],surveys_combined.mfa$quali.var$coord[,2]) 
 
-# 1) Calculate loadings from coordinates 
+# 1) Calculate loadings from coordinates and print loadings
 # 2) Rotate values 
 # 3) Load rotated values into previous values
 loadings.mfa <- sweep(surveys_combined.mfa$quali.var$coord,2,
                       sqrt(surveys_combined.mfa$eig[1:ncol(surveys_combined.mfa$quali.var$coord),1]),FUN="/")
+loadings <- cbind(loadings.mfa[,1],loadings.mfa[,2]) 
 loadings.mfa.rot <- varimax(loadings.mfa)$loadings
 surveys_combined.mfa$quali.var$coord <- loadings.mfa.rot
 
 # Plot of rotated loadings
 rotated_plot_dim1 <- barplot(surveys_combined.mfa$quali.var$coord[,1], las=2)
 rotated_plot_dim2 <- barplot(surveys_combined.mfa$quali.var$coord[,2], las=2)
+rot_loadings <- cbind(surveys_combined.mfa$quali.var$coord[,1],surveys_combined.mfa$quali.var$coord[,2]) 
+
+# Write loadings to csv
+all_loadings <- cbind(coordinates, loadings,rot_loadings)
+colnames(all_loadings) <- c("coord.dim1","coord.dim2","load.dim1","load.dim2","rotLoad.dim1","rotLoad.dim2")
+write.csv(all_loadings,file="all_loadings.csv")
 
 # Extract row results
 eig.val <- get_eigenvalue(surveys_combined.mfa)
