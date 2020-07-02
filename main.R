b8fbfcaa72b8b5a4a8353b8250623ac80f2f80a5
jborycz <amanofsorrows@gmail.com>
2020-07-02 15:46:49 -0500

    Incorporated rkolendo scripts

--- main.R
+++ main.R
@@ -90,7 +90,7 @@ source("scripts/multiFactorAnalysis.R")
 # source("scripts/archive/statsSurveyComp.R")
  
 # PLOTS: Create dimension 1 and 2 average plots used in publication
-# source("scripts/archive/publication_plots.R")
+source("scripts/archive/publication_plots.R")
 
 # Print loadings to /data/all_loadings.csv
 # Print list  of most correlated variables to
